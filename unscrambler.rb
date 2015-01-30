37de0c9cd155276ff28a02a211e4fc74e1d2b57c
Vijay Ananth <vijay.hassan@gmail.com>
2015-01-30 21:55:55 +0530

    basic test syntax

--- unscrambler.rb
+++ unscrambler.rb
@@ -2,4 +2,16 @@ require 'tree'
 
 class Unscrambler 
 
+  def initialize
+    @l = []
+  end
+
+
+  def add s
+    @l << s
+  end
+
+  def get s
+    @l
+  end
 end
--- unscrambler_spec.rb
+++ unscrambler_spec.rb
@@ -6,5 +6,12 @@ RSpec::Given.use_natural_assertions
 describe Unscrambler do
   Given(:unscrambler) {Unscrambler.new}
 
-  Then  {unscrambler != nil}
+  describe  'initialisation' do 
+    Then  {unscrambler != nil}
+  end
+
+  describe 'insert "a"' do
+    When {unscrambler.add("a")}
+    Then { expect(unscrambler.get("a")).to eq(["a"])}
+  end
 end
